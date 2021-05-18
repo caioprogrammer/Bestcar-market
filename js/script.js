36472b75e16fc485e372dfa9b24d03e7e7addaa8
Caio Nogueira <81397501+caioprogrammer@users.noreply.github.com>
2021-05-18 19:04:52 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function captura() {
+    const menu = document.querySelector('.options-behind')
+    
+    menu.style.display = 'block';
+    menu.style.transition = 'all .5s'
+}
+ function remove() {
+    const menu = document.querySelector('.options-behind')
+    
+    menu.style.display = 'none';
+}
+ 
---
+++ js/teste.js
@@ -0,0 +1,10 @@
+let itemsJSON = [
+    {id:1, name:'10', img:'assets/images/volante-esportivo-golf-gti-vision-universal-grafite-com-acionador-buzina-connectparts--1--1.png', price:'3.799,90',  description:'ou 10x de R$ 379,99'},
+    {id:2, name:'20', img:'assets/images/par-alto-falante-bravox-b3x50x-5-100w-rms-4-ohms-triaxial-connectparts--1--1.png', price:'29.799,90', description:'ou 10x de R$ 2.979,99'},
+    {id:3, name:'30', img:'assets/images/Bola-De-Cambio-Manopla-Peugeot-206-207-208-307-308-01-18-Preta-Cinza-Preta-Lateral-Cinza-connectparts---1-.png', price:'79,90', description:'ou 10x de R$ 7,99'},
+    {id:4, name:'40', img:'assets/images/calota-aro-13-14-15-ds4-black-universal-connectparts--1-.png', price:'37,90',  description:'ou 10x de R$ 3,79'},
+    {id:5, name:'50', img:'assets/images/Bola-De-Cambio-Manopla-Peugeot-206-207-208-307-308-01-18-Preta-Cinza-Preta-Lateral-Cinza-connectparts---1-.png', price:'79,90', description:'ou 10x de R$ 7,99'},
+    {id:6, name:'60', img:'assets/images/calota-aro-13-14-15-ds4-black-universal-connectparts--1-.png', price:'37,90', description:'ou 10x de R$ 3,79'},
+    {id:7, name:'80', img:'assets/images/volante-esportivo-golf-gti-vision-universal-grafite-com-acionador-buzina-connectparts--1--1.png', price:'3.799,90', description:'ou 10x de R$ 379,99'},
+    {id:8, name:'90', img:'assets/images/par-alto-falante-bravox-b3x50x-5-100w-rms-4-ohms-triaxial-connectparts--1--1.png', price:'29.799,90', description:'ou 10x de R$ 2.979,99'},
+]
